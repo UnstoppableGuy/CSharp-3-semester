973f4f91341b5c71b6ede3aef87482cae06d9b58
Pavel <pavel.takynov@gmail.com>
2020-12-15 23:23:10 +0300

    lab4

---
+++ lab4/DataManager.cs
@@ -0,0 +1,32 @@
+﻿using System.ServiceProcess;
+using HelpLibrary;
+
+namespace Lab4
+{
+    public partial class DataManager : ServiceBase
+    {
+        readonly DataBaseWorker appInsights;
+        readonly DataOptions dataOptions;
+        public DataManager(DataOptions dataOptions, DataBaseWorker appInsights)
+        {
+            InitializeComponent();
+            this.dataOptions = dataOptions;
+            this.appInsights = appInsights;
+        }
+        protected override void OnStart(string[] args)
+        {
+            DataBaseWorker reader = new DataBaseWorker(dataOptions.ConnectionString);
+            FileTransfer fileTransfer = new FileTransfer(dataOptions.OutputFolder, dataOptions.SourcePath);
+            string customersFileName = "customers";
+            reader.GetCustomers(dataOptions.OutputFolder, appInsights, customersFileName);
+            fileTransfer.SendFileToFtp($"{customersFileName}.xml");
+            fileTransfer.SendFileToFtp($"{customersFileName}.xsd");
+            appInsights.InsertInsight("Files uploaded");
+        }
+        protected override void OnStop()
+        {
+            appInsights.InsertInsight("Stop");
+            appInsights.WriteInsightsToXml(dataOptions.OutputFolder);
+        }
+    }
+}
---
+++ lab4/HelpLibrary/DataClasses/XmlGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.IO;
+
+namespace HelpLibrary
+{
+    public class XmlGenerator
+    {
+        readonly string outputFolder;
+        public XmlGenerator(string outputFolder)
+        {
+            this.outputFolder = outputFolder;
+        }
+        public void WriteToXml(DataSet dataSet, string fileName)
+        {
+            dataSet.WriteXml(Path.Combine(outputFolder, $"{fileName}.xml"));
+            dataSet.WriteXmlSchema(Path.Combine(outputFolder, $"{fileName}.xsd"));
+        }
+    }
+}
